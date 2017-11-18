1ff47318e6b5d2145f7eca979b285bc4ff2df5d8
peterbuild <peterbuild@gmail.com>
2017-11-18 20:38:47 +0200

    start work. I requested data and created two output options

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter Your Name: ");
+        String name = in.nextLine();
+
+        System.out.print("Enter the city where you are located: ");
+        String locate = in.nextLine();
+
+        System.out.print("Enter Your age: ");
+        String str;
+        int age;
+        while(true) {
+            str = in.nextLine();
+            try {
+                age = Integer.parseInt(str);
+                break;
+            } catch (Exception e) {
+                System.err.print(str + " not age!!! Please enter integer: ");
+            }
+        }
+        System.out.print("Enter Your hobby: ");
+        String hobby = in.nextLine();
+        //output1
+        System.out.println();
+        System.out.println("Name:       " + name);
+        System.out.println("City:       " + locate);
+        System.out.println("Age:        " + age);
+        System.out.println("Hobby:      " + hobby);
+        ///output2
+        System.out.println();
+        System.out.println("A man named " + name + " lives in the city "+ locate + ".\nThis person " + age +" years and he loves to engage in " + hobby +".");
+
+
+
+    }
+}
